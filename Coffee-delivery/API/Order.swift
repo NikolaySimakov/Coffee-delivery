759ccf267c8f09b59c3a52153f134e8b5c531ca0
NikolaySimakov <n.simakoff@gmail.com>
2021-09-09 21:38:56 +0300

    order data

--- Coffee-delivery/API/Order.swift
+++ Coffee-delivery/API/Order.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class Order : NSObject {
+    
+    var cafeTitle : String!
+    var totalPrice : String!
+    var deliveryDate : Date?
+    var goods : [Product]!
+    
+    init(cafeTitle: String, totalPrice: String, deliveryDate: Date? = nil, goods: [Product]) {
+        self.cafeTitle = cafeTitle
+        self.totalPrice = totalPrice
+        self.deliveryDate = deliveryDate
+        self.goods = goods
+    }
+    
+    func deliveryDateString() -> String {
+        if self.deliveryDate == nil { return "В процессе доставки" }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        let day = dateFormatter.string(from: self.deliveryDate!)
+        dateFormatter.dateFormat = "HH:mm"
+        let time = dateFormatter.string(from: self.deliveryDate!)
+        
+        return "Доставлено в \(time), \(day)"
+    }
+    
+}
