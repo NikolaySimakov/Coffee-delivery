3d94d50174d7a7ed48543a9d9c23ed0ea2e67681
NikolaySimakov <n.simakoff@gmail.com>
2021-09-09 21:50:31 +0300

    functions: save, content, remove/removeAll, update

--- Coffee-delivery/CoreData/Storages/CartStorage.swift
+++ Coffee-delivery/CoreData/Storages/CartStorage.swift
@@ -6,13 +6,145 @@
 //  Copyright © 2021 Nikolay Simakov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 class CartStorage {
     
-    func updateCart() {
+    private var context: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    func update( _ cafeId: Int, _ id: Int, _ quantity: Int) {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Cart")
         
+        do {
+            
+            let managedObjects = try context.fetch(fetchRequest)
+            
+            for obj in managedObjects {
+                
+                if cafeId == (obj.value(forKey: "cafeId") as? Int)! && id == (obj.value(forKey: "id") as? Int)! {
+                    
+                    obj.setValue(quantity, forKey: "count")
+                    
+                    saveContext()
+                }
+                
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func save(products : [Product], for cafeId: Int) {
+        
+        for product in products {
+            
+            let entity = NSEntityDescription.entity(forEntityName: "Cart", in: context)!
+            let cart = NSManagedObject(entity: entity, insertInto: context)
+            
+            cart.setValue(product.title, forKeyPath: "title")
+            cart.setValue(product.price, forKeyPath: "price")
+            cart.setValue(product.inCart(), forKeyPath: "count")
+            cart.setValue(product.id, forKeyPath: "id")
+            cart.setValue(cafeId, forKeyPath: "cafeId")
+            
+            saveContext()
+            
+        }
+        
+    }
+    
+    
+    // returns [index : count]
+    
+    func content(for cafeId: Int) -> [Int : Int]? {
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Cart")
+        
+        do {
+            
+            let managedObjects = try context.fetch(fetchRequest)
+            
+            if managedObjects.count != 0 {
+                
+                var quantities : [Int : Int] = [:]
+                
+                for i in managedObjects {
+                    
+                    if cafeId == (i.value(forKey: "cafeId") as? Int)! {
+                        
+                        let _id = (i.value(forKey: "id") as? Int)!
+                        let quantity = (i.value(forKey: "count") as? Int)!
+                        
+                        quantities[_id] = quantity
+                        
+                    }
+                    
+                }
+                
+                return quantities
+            } else { return nil }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        return nil
+    }
+    
+    func remove(product : NSManagedObject) {
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Cart")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            for obj in try context.fetch(fetchRequest) as! [NSManagedObject] {
+                if product == obj {
+                    context.delete(obj)
+                }
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        saveContext()
+    }
+    
+    func removeAll() {
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Cart")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            for obj in try context.fetch(fetchRequest) as! [NSManagedObject] {
+                context.delete(obj)
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        saveContext()
+        
+    }
+    
+    private func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+              context.rollback()
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
     
 }
