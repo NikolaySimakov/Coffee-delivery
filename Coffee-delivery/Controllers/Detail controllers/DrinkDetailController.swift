edfd2376ee2876b6c4b6f22ebe6e877f7b526ef4
NikolaySimakov <n.simakoff@gmail.com>
2021-09-10 01:44:59 +0300

    added ± buttons, delegate, shortened code

--- Coffee-delivery/Controllers/Detail controllers/DrinkDetailController.swift
+++ Coffee-delivery/Controllers/Detail controllers/DrinkDetailController.swift
@@ -1,5 +1,5 @@
 //
-//  DetailController.swift
+//  DrinkDetailController.swift
 //  Coffee-delivery
 //
 //  Created by User on 20/07/2021.
@@ -8,31 +8,55 @@
 
 import UIKit
 
-class DetailController: UIViewController {
+class DrinkDetailController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
-    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var priceBtn: UIButton!
+    @IBOutlet weak var increaseBtn: UIButton!
+    @IBOutlet weak var decreaseBtn: UIButton!
     
-    @IBAction func addToCart(_ sender: UIButton) {
-        self.dismiss(animated: true) {
-            print("something")
+    @IBAction func increase(_ sender: UIButton) {
+        product.add()
+        let productQuantity = product.inCart() + 1
+        self.priceBtn.setTitle("Добавить \(productQuantity * Int(product.price)!)₽ (\(productQuantity) шт.)", for: .normal)
+    }
+    
+    @IBAction func decrease(_ sender: UIButton) {
+        product.subtract()
+        let productQuantity = product.inCart() + 1
+        if productQuantity == 1 {
+            self.priceBtn.setTitle("Добавить \(product.price!)₽", for: .normal)
+        } else {
+            self.priceBtn.setTitle("Добавить \(productQuantity * Int(product.price)!)₽ (\(productQuantity) шт.)", for: .normal)
         }
     }
     
+    @IBAction func addToCart(_ sender: UIButton) {
+        if let delegate = self.delegate {
+            delegate.addProductToCart(productId: product.id)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
     
-    var _image = UIImage()
-    var _title = String()
-    var _description = String()
+    
+    var datailDescription : String!
+    var product : Product!
+    
+    var delegate : ProductDelegate?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addButton.layer.cornerRadius = 10
-        self.imageView.image = _image
-        self.titleLabel.text = _title
+        priceBtn.layer.cornerRadius = 10
+        increaseBtn.layer.cornerRadius = 10
+        decreaseBtn.layer.cornerRadius = 10
+        
+        self.imageView.image = product.image
+        self.titleLabel.text = product.title
+        self.priceBtn.setTitle("Добавить \(product.price!)₽", for: .normal)
     }
 
 }
