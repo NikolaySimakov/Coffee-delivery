61ef9d43c00fb0b309f296214dedc2f1b338400c
NikolaySimakov <n.simakoff@gmail.com>
2021-09-10 13:27:07 +0300

    new functions: loadImageIfNeeded, addToCart, subtractFromCart, showOrderBtn (when cart is not empty), hideOrderBtn (when cart is empty); added ProductDelegate for Detail controller (to add product from DrinkDetailController); customized cell sizes and spaces

--- Coffee-delivery/Controllers/MenuController.swift
+++ Coffee-delivery/Controllers/MenuController.swift
@@ -7,25 +7,31 @@
 //
 
 import UIKit
-import CoreData
 
 
-private let reuseIdentifier = "productCell"
+protocol ProductDelegate {
+    func addProductToCart(productId : Int)
+}
 
 
 class MenuController: UIViewController {
     
-    private var goods : [Int : Product] = [:] // index : product
-    private var cart : [Int : (Product, Int)] = [:] // index : [product, count]
-    private var goodsInCart : [Array<Any>] = []
-    var menuID : String!
+    private let parser = CoffeeParser()
+    private var cartStorage = CartStorage()
     
-    lazy var searchBar: UISearchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 200, height: 20))
+    private var goods : [Int : Product] = [:] // index : product
+    var cart : [Int : Product] = [:]
+    var menuID : String!
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var orderBtn: UIButton!
     @IBOutlet weak var searchItem: UIBarButtonItem!
     
+    // constraints
+    @IBOutlet weak var orderBtnBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var orderBtnViewHeightConstraint: NSLayoutConstraint!
+    
+    private let collectionLayout = UICollectionViewFlowLayout()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,61 +39,42 @@ class MenuController: UIViewController {
         orderBtn.layer.cornerRadius = 10
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.collectionViewLayout = collectionLayout
+        collectionView.scrollIndicatorInsets = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         
-        let parser = CoffeeParser()
+        orderBtnBottomConstraint.constant = -orderBtnViewHeightConstraint.constant
+        
         parser.fetchGoods(menuID: menuID, { goods in
             self.goods = goods
+            self.initCart()
             self.collectionView.reloadData()
         })
-        
-        initCart()
     }
     
-    
-    func initCart() {
-        
-        guard let appDelegate =
-            UIApplication.shared.delegate as? AppDelegate else {
-                return
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "UserInfo")
+    private func initCart() {
+        if let quantities = cartStorage.content(for: Int(menuID)!) {
+            if quantities.count == 0 {
+                
+                let alert = UIAlertController(title: "Очистить корзину?", message: "В вашей корзине находятся товары из другой кофейни, поэтому её необходимо очистить.", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Назад", style: .cancel, handler: { (_) in
+                    self.navigationController?.popViewController(animated: true)
+                }))
+                
+                alert.addAction(UIAlertAction(title: "Ок", style: .default, handler: { (_) in
+                    self.cartStorage.removeAll()
+                }))
 
-        
-        do {
-            for i in try managedContext.fetch(fetchRequest) {
-                goodsInCart.append(
-                    (i.value(forKey: "currentCart") as? Array<Any>)!
-                )
+                self.present(alert, animated: true)
+                
+            } else {
+                showOrderBtn()
+                quantities.forEach { (key, value) in
+                    cart[key] = goods[key]
+                    cart[key]?.add(value)
+                }
             }
-            
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
-    }
-    
-    
-    private func addProductToCart(index: Int, count: Int) {
-      
-        guard let appDelegate =
-            UIApplication.shared.delegate as? AppDelegate else {
-                return
-        }
-      
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let entity = NSEntityDescription.entity(forEntityName: "UserInfo", in: managedContext)!
-        let cart = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        let productTitle = goods[index]!.title
-        let productPrice = goods[index]!.price
-        cart.setValue([productTitle, productPrice, count], forKeyPath: "currentCart")
-      
-        do {
-            try managedContext.save()
-            print("Added successfully")
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
         }
     }
 
@@ -95,20 +82,34 @@ class MenuController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as?
             CartController {
-            for data in cart {
-                destination.cart.append(data.value)
+            
+            for (_, product) in cart {
+                destination.cart.append(product)
             }
-        } else if let destination = segue.destination as? DetailController {
-            if let item = collectionView.indexPathsForSelectedItems?.last?.row {
-                let product = goods[item]!
-                destination._title = product.title
-                destination._image = (collectionView.cellForItem(at: IndexPath(item: item, section: 0)) as! ProductCell).productImageView.image!
-            }
+            
+        } else if let destination = segue.destination as? DrinkDetailsController, let item = collectionView.indexPathsForSelectedItems?.last?.row {
+            destination.product = goods[item]!
+            destination.delegate = self
+        } else if let destination = segue.destination as? SearchProductsController {
+            destination.searchGoods = [Product](goods.values)
+            destination.detailDelegate = self
         }
     }
 }
 
-extension MenuController: UICollectionViewDataSource {
+
+extension MenuController: ProductDelegate {
+    
+    func addProductToCart(productId: Int) {
+        self.addToCart(productId)
+        self.collectionView.reloadItems(at: [IndexPath(item: productId, section: 0)])
+    }
+    
+    
+}
+
+
+extension MenuController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -119,63 +120,106 @@ extension MenuController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProductCell
         
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCell.ID, for: indexPath) as! ProductCell
         
-        cell.layer.cornerRadius = 20
-        cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         if cart.keys.contains(indexPath.row) {
-            cell.countLabel.text = String(self.cart[indexPath.row]!.1)
-            cell.initData(product: goods[indexPath.row]!, count: self.cart[indexPath.row]!.1)
+            cell.countLabel.text = String(self.cart[indexPath.row]!.inCart())
+            cell.initData(product: cart[indexPath.row]!)
         } else {
             cell.initData(product: goods[indexPath.row]!)
         }
-
-//        for g in goodsInCart {
-//            if g[0] as! String == goods[indexPath.row].title {
-//                cell.priceBtn.setTitle("Добавлено", for: .normal)
-//                cart[indexPath.row] = g[2] as? Int
-//            }
-//        }
         
-        cell.addProductAction = {
-//            if self.cart.keys.contains(indexPath.row) {
-                self.cart[indexPath.row] = (self.goods[indexPath.row]!, 1)
-                
-//                self.addProductToCart(index: indexPath.row, count: 1)
-//            }
-            print(self.cart)
-        }
+        loadImageIfNeeded(array: goods, item: indexPath.row, cell: cell)
         
-        cell.increaseCountAction = {
-            let product = self.cart[indexPath.row]!.0
-            let count = self.cart[indexPath.row]!.1 + 1
-            self.cart[indexPath.row] = (product, count)
-            collectionView.reloadItems(at: [indexPath])
-        }
+        // actions
         
-        cell.decreaseCountAction = {
-            if self.cart.keys.contains(indexPath.row) {
-                let product = self.cart[indexPath.row]!.0
-                let count = max(self.cart[indexPath.row]!.1 - 1, 0)
-                self.cart[indexPath.row] = (product, count)
-                collectionView.reloadItems(at: [indexPath])
-            }
-            
-            if self.cart[indexPath.row]!.1 == 0 {
-                let count = self.cart[indexPath.row]!.1
-                cell.btnsMapping(count)
-                self.cart.removeValue(forKey: indexPath.row)
-            }
-        }
+        cell.addProductAction = { self.addToCart(indexPath.row) }
+        cell.increaseCountAction = { self.addToCart(indexPath.row) }
+        cell.decreaseCountAction = { self.subtractFromCart(indexPath.row, cell) }
     
         return cell
     }
+    
+    private func loadImageIfNeeded(array: [Int : Product], item: Int, cell: ProductCell) {
+        if let img = array[item]!.image {
+            cell.productImageView.image = img
+        } else {
+            parser.getImage(array[item]!.imageURL!) { (img) in
+                array[item]!.image = img
+                self.collectionView.reloadItems(at: [IndexPath(item: item, section: 0)])
+            }
+        }
+    }
 
-}
-
-
-extension MenuController: UICollectionViewDelegate {
+    
+    private func addToCart( _ productId : Int) {
+        
+        if self.cart.keys.contains(productId) {
+            self.cart[productId]!.add()
+            cartStorage.update(Int(menuID)!, productId, self.cart[productId]!.inCart())
+            collectionView.reloadItems(at: [IndexPath(row: productId, section: 0)])
+        } else {
+            if cart.count == 0 { showOrderBtn() }
+            cart[productId] = goods[productId]!
+            cart[productId]?.add()
+        }
+        
+        cartStorage.removeAll()
+        cartStorage.save(products: [Product](self.cart.values), for: Int(menuID)!)
+        
+    }
+    
+    private func subtractFromCart( _ productId : Int, _ cell: ProductCell) {
+        if self.cart.keys.contains(productId) {
+            self.cart[productId]!.subtract()
+            collectionView.reloadItems(at: [IndexPath(row: productId, section: 0)])
+        }
+        
+        if self.cart[productId]!.removeFromCart {
+            cell.btnsMapping(self.cart[productId]!.inCart())
+            self.cart.removeValue(forKey: productId)
+            if cart.count == 0 { hideOrderBtn() }
+        }
+        
+        cartStorage.removeAll()
+        cartStorage.save(products: [Product](self.cart.values), for: Int(menuID)!)
+    }
+    
+    private func showOrderBtn() {
+        
+        orderBtnBottomConstraint.constant = 0
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: orderBtnViewHeightConstraint.constant, right: 20)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    private func hideOrderBtn() {
+        
+        orderBtnBottomConstraint.constant = -orderBtnViewHeightConstraint.constant
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: { (_) in
+            UIView.animate(withDuration: 0.3) {
+                self.collectionView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+            }
+        })
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 175, height: 320)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     }
